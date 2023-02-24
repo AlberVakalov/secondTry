f1a17898872b2b2a73ba16f6c56476f95a6e2779
Your Name <Vakalovaelbert@yandex.ru>
2023-02-24 15:04:32 +0300

    Revert "Add day 5, task 2"
    This reverts commit 80357bb82f866021adb20e331730bc8f1e08e43d.

--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -2,15 +2,6 @@ package day6;
 
 public class Task2 {
     public static void main(String[] args) {
-    Airplane airplane = new Airplane("Самолет1",1925, 100,1000);
 
-
-    airplane.setManufacture("Самолет 3");
-    airplane.setYear(2005);
-    airplane.fillUp(10000);
-    airplane.setLength(1000);
-    airplane.setWeight(200000);
-
-    airplane.info();
     }
 }
--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -2,9 +2,6 @@ package day6;
 
 public class Task3 {
     public static void main(String[] args) {
-    Teacher teacher = new Teacher("Валентина Степановна", "Математика");
-            Student student1 = new Student("Антон Дмитрыч", "Биология");
 
-            teacher.evaluate(student1);
     }
 }
