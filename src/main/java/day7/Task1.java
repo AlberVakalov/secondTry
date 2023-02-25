dc0a292a268dcf07830bd9a99aa11578f7c64312
Your Name <Vakalovaelbert@yandex.ru>
2023-02-25 07:15:08 +0300

    add day 7, all tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+    Airplane boeing = new Airplane("BOEING", 2010, 100000, 20000000);
+    Airplane pobeda = new Airplane("POBEDA", 2000, 10000, 180000);
+    Airplane.compareAirplanes(boeing, pobeda);
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,28 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+
+        Player akinfeev = new Player(100);
+        Player dzyba = new Player(100);
+        Player ronaldo = new Player(100);
+
+        Player.info();
+
+        Player andreev = new Player(95);
+        Player belkin = new Player(95);
+        Player murativ = new Player(95);
+        Player.info();
+
+        for (int i = 0; i < 100; i++) {
+            ronaldo.run();
+            dzyba.run();
+        }
+
+        Player.info();
 
     }
 }
