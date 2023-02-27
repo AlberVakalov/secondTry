cd7db31bd537755235bd375c447c7228b955f4ad
Your Name <Vakalovaelbert@yandex.ru>
2023-02-27 17:59:31 +0300

    Add day 8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,65 @@
+package day8;
+
+public class Airplane {
+
+    String manufacture;
+    int year;
+    int length;
+    int weight;
+    int fuel;
+
+    public Airplane(String manufacture, int year, int weight, int length) {
+
+    }
+
+
+    public void fillUp(int fuel) {
+        this.fuel = fuel;
+    }
+
+
+    public void setFuel(int inFuel) {
+        fuel += inFuel;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + manufacture + ", год выпуска: " + year + ", длина: " + length +
+                " " + ", вес: " + weight + ", кол-во топлива: " + fuel);
+    }
+
+    public static void compareAirplanes(Airplane air1, Airplane air2) {
+        if (air1.getLength() > air2.getLength())
+            System.out.println("Длинее первый самолет");
+        else
+            System.out.println("Длинее второй самолет");
+    }
+    public String toString(){
+        return manufacture + " " + year + " " + weight + " " + length;
+    }
+
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,33 @@
 package day8;
 
 public class Task1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        String x = " ";
+        for (int i = 0; i <= 20000; i++)
+            x += i + " ";
+
+
+        System.out.println(x);
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("Длительность работы, в мс: " + (stopTime - startTime));
+
+
+        long startTimer = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(" ");
+        for (int i = 0; i <= 20000; i++)
+            sb.append(i).append(" ");
+
+        System.out.println(sb);
+
+        long stopTimer = System.currentTimeMillis();
+
+        System.out.println("Длительность работы, в мс: " + (stopTimer - startTimer));
+
+        //There is a 476ms difference
     }
+
 }
