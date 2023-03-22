df3a83774c5741286f18ecbd609ff99409365fa9
Your Name <Vakalovaelbert@yandex.ru>
2023-03-22 22:27:37 +0300

    Add day 16, first task

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,31 @@
 package day16;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+    File file = new File("C:\\Users\\Alik\\IdeaProjects\\secondTry\\src\\main\\java\\day16\\NUMBERS");
+        System.out.println(printResult(file));
     }
+    public static double printResult(File file) throws FileNotFoundException {
+        double sum = 0;
+        double scale = Math.pow(10, 3);
+
+        try {
+            Scanner sc = new Scanner(file);
+            String[] numbers = sc.nextLine().split(" ");
+            sum = 0;
+            for (String x : numbers) {
+                sum += Integer.parseInt(x);
+            }
+            sum = sum / numbers.length;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return Math.ceil(sum * scale) / scale;
+    }
+
+
 }
 
